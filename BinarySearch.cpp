1ab084381ca31fa5096c317a15d1717fd17085b2
rainnasyahda8 <rainnasyahda8@gmail.com>
2026-04-17 15:29:46 +0700

    menambahkan low

--- BinarySearch.cpp
+++ BinarySearch.cpp
@@ -11,5 +11,97 @@ void input()
     {
         cout << "Masukkan banyaknya elemen pada array (maksimal 10) : ";
         cin >> nPanjang;
+
+        if (nPanjang <= 10)
+        {
+            break;
+        }
+        else
+        {
+            cout << "\n[!] jumlah elemen tidak boleh lebih dari 10. Silahkan coba lagi. \n";
+        }
+    }
+
+    cout << "\n==================================\n";
+    cout << "       Masukkan Elemen Array        \n";
+    cout << "\n==================================\n";
+
+    for (int i = 0; i < nPanjang; i++)
+    {
+        cout <<"Data ke - " << (i + 1) << " = ";
+        cin >> element[i];
+    }
+}
+
+void bubbleSortArray()
+{
+    int pass = 1;
+    do
+    {
+        for (int j = 0; j <= nPanjang - 1 - pass; j++)
+        {
+            if (elemen[j] > elemen[j + 1])
+            {
+                int temp = element[j];
+                element[j] = element[j + 1];
+                element[j + 1] = temp;
+            }
+        }
+        pass++;
+    } while (pass <= nPanjang - 1);
+}
+
+void display()
+{
+    cout << "\n====================================\n";
+    cout << "  Elemen Array Setelah Diurutkan (Asc)\n";
+    cout << "\n====================================\n";
+
+    for (int j = 0; j < nPanjang; j++)
+    {
+        cout << element[j];
+        if (j < nPanjang - 1)
+        {
+            cout << " -> ";
+        }
+    }
+    cout << endl;
+}
+
+void binarySearch()
+{
+    char ulang;
+    do
+    {
+        cout << "\n====================================\n";
+        cout << "         Pencarian Binary Search      \n";
+        cout << "\n====================================\n";
+
+        cout << "Masukkan elemen yang ingin dicari : ";
+        cin >> x;
+
+        int low = 0;
+        int high = nPanjang - 1;
+
+        do
+        {
+            int mid = (mid + high) / 2;
+
+            if (element[mid] == x)
+            {
+                cout << "\n[✓] Elemen " << x << " ditemukan pada ideks " << mid << "\n";
+                return;
+            }
+
+            if (x < element[mid])
+            {
+                high = mid - 1; 
+            }
+
+            if (x > element[mid])
+            {
+                low = mid + 1;
+            }
+        } 
     }
 }
